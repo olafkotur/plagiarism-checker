3293ae33c05e5af936c491bcf4fed7394461c3b4
Olaf Kotur <olafkotur97@gmail.com>
2018-04-29 13:17:20 +0100

    Results are now displayed in a generated html file

--- Display.java
+++ Display.java
@@ -1,16 +1,45 @@
 import java.io.*;
+import java.util.*;
 
 public class Display {
-	
+
+	Load load = new Load();
+	Frequency frequency = new Frequency();
+
+	private ArrayList <String> tempList = new ArrayList <String>();
+	private String tableCells;
+		
+	// Generate a template HTML whilst calling populateTable method to find correct values
 	public void generateHTML() throws IOException {
-		File file = new File("test.html");
+		File file = new File("results.html");
 		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+		frequency.getUserRequest();
+		frequency.frequencyCheck(frequency.getOriginalIndex(), frequency.getCompareIndex());
+		load.readParagraph();
+		populateTable();
 
+		String tableHeader = "<tr><th>Word</th><th>Original</th><th>Comparison</th>" + tableCells + "</tr>";
+		String tableMain = "<div><h2>Word Frequency</h2><table style=\"width: 50%; text-align: left;\">" + tableHeader + "</table></div>";
 		String title = "<div style=\"text-align: center; margin-top: 50px\"><h1>Plagiarism Checker Results</h1></div>";
-		String tableMain = "<div><h2>Word Frequency</h2><table style=\"width: 50%; text-align: left;\"><tr>" + </tr>";
 
-		writer.write(title);
-		writer.write(tableMain);
+		String html = title + tableMain;
+
+		writer.write(html);
 		writer.close();
+	}
+
+	// Find frequency values for each word mentioned in the files
+	public void populateTable() throws IOException {
+		String resultsName;
+		String resultsOriginal;
+		String resultsCompare;
+		load.toWords(frequency.getOriginalIndex());
+		for (int i = 0; i < load.getWords().size(); i++) {
+			resultsName = "<tr><td>" + load.getSingleWord(i) + "</td>";
+			resultsOriginal = "<td>" + frequency.getFrequencyOne(i) + "</td>"; 
+			resultsCompare = "<td>" + frequency.getFrequencyTwo(i) + "</td></tr>"; 
+			tempList.add(resultsName + resultsOriginal + resultsCompare);
+		}
+		tableCells = tempList.toString().replaceAll("[ |,|\\[|\\]]", "");
 	}
 }
--- Frequency.java
+++ Frequency.java
@@ -4,21 +4,62 @@ import java.util.*;
 public class Frequency {
 
 	Load load = new Load();
+	Load reload = new Load();
 
-	ArrayList <Integer> frequencyList = new ArrayList <Integer>();
+	ArrayList <Integer> frequencyOneList = new ArrayList <Integer>();
+	ArrayList <Integer> frequencyTwoList = new ArrayList <Integer>();
+	int originalIndex = 0;
+	int compareIndex = 0;
 
-	public void frequencyCheck() throws IOException {
+	// Read both files in to separate instances, find the word frequency of the original
+	// file and compare it to the second one.
+	public void frequencyCheck(int indexOne, int indexTwo) throws IOException {
+		
+		// Storing the words of both files into memory
 		load.readParagraph();
-		for (int i = 0; i < 5; i++) {
-			int count = 0;
-			load.toWords(i);
-			for (int j = 0; j < load.getWords().size(); j++) {
-				count = Collections.frequency(load.getWords(), load.getSingleWord(j));
-				frequencyList.add(count);
-			}
-			System.out.println(frequencyList + "\n\n");
-			frequencyList.clear();
-			load.clearWordList();
+		load.toWords(indexOne);
+		reload.readParagraph();
+		reload.toWords(indexTwo);
+
+		// Check for occurence of each word and store in separate ArrayLists
+		int countOne = 0;
+		int countTwo = 0;
+		for (int i = 0; i < load.getWords().size(); i++) {
+			countOne = Collections.frequency(load.getWords(), load.getSingleWord(i));
+			countTwo = Collections.frequency(reload.getWords(), load.getSingleWord(i));
+			frequencyOneList.add(countOne);
+			frequencyTwoList.add(countTwo);
 		}
+	}
+
+	// Ask user which two files they would like to compare in the frequency check
+	public void getUserRequest() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter index number of the original file (1 - 5): ");
+		originalIndex = scanner.nextInt() - 1;
+		
+		System.out.println("Please enter index number of the comparison file (1 - 5): ");
+		compareIndex = scanner.nextInt() - 1;
+		scanner.close();
+	}
+
+	// Retrieve the occurences of each word in the original file
+	public int getFrequencyOne(int index) {
+		return frequencyOneList.get(index);
+	}
+
+	// Retrieve the occurences of each word in the comparison file
+	public int getFrequencyTwo(int index) {
+		return frequencyTwoList.get(index);
+	}
+
+	// Retrieve the value of the original file requested by user
+	public int getOriginalIndex() {
+		return originalIndex;
+	}
+
+	// Retrieve the value of the comparison file requested by user
+	public int getCompareIndex() {
+		return compareIndex;
 	}
 }
