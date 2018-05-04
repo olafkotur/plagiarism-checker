88a1fbd7a6b06aa75d93c5b117edd8d7fd985d40
Olaf Kotur <olafkotur97@gmail.com>
2018-05-04 21:14:24 +0100

    Fixed minor bugs, removed test code, improved comments and included progress updates as System.out in terminal

--- Display.java
+++ Display.java
@@ -3,15 +3,24 @@ import java.util.*;
 
 public class Display {
 
+	// New instances of classes used
 	Load load = new Load();
 	Frequency frequency = new Frequency();
 	Match match = new Match();
 
+	// Initialising Arraylists
 	private ArrayList <String> tempList = new ArrayList <String>();
+	private ArrayList <String> tableOne = new ArrayList <String>();
+	private ArrayList <String> tableTwo = new ArrayList <String>();
+	private ArrayList <String> tableThree = new ArrayList <String>();
 	private ArrayList <String> wordList = new ArrayList <String>();
+
+	// Initialising Strings
 	private String frequencyTableCells;
+	private String phraseDataCells;
 		
-	// Generate a template HTML whilst calling populateTable method to find correct values
+	// Generate two HTML files with the provided template, uses populateFrequencyTable() and populateMatchTable() methods
+	// to receive data on the files. 
 	public void generateHTML() throws IOException {
 		File frequencyFile = new File("frequency.html");
 		BufferedWriter writerOne = new BufferedWriter(new FileWriter(frequencyFile));
@@ -22,28 +31,41 @@ public class Display {
 		frequency.getUserRequest();
 		int original = frequency.getOriginalIndex();
 		int compare = frequency.getCompareIndex();
+		System.out.println("\n--------- Reading file " + original + " and file " + compare);
 
+		// Populate frequency and phrase tables
 		load.readParagraph();
 		frequency.frequencyCheck(original, compare);
+		System.out.println("\n--------- Calculating Word Frequency");
 		populateFrequencyTable(original);
+		System.out.println("\n--------- Frequency table Complete");
 		populateMatchTable();
+		System.out.println("\n--------- Phrase Matching Complete");
 
+		// Template
 		String title = "<div style=\"text-align: center; margin-top: 50px\"><h1>Plagiarism Checker Results</h1></div>";
 		String tableHeader = "<tr><th>Word</th><th>File - " + (original+1) + "</th><th>File - " + (compare+1) + "</th>" + frequencyTableCells + "</tr>";
 		String links = "<div style=\"text-align: center;\"><h2><a href=\"frequency.html\" style=\"display: inline; margin: 20px;\">Word Frequency</a><a href=\"phrase.html\" style=\"display: inline; margin: 20px;\">Phrase Matching</a></h2></div>";
 		String tableMain = "<div style=\"margin: auto; width:50%;\">" + links + "<table border=\"1\"style=\"width: 100%; text-align: left;\">" + tableHeader + "</table></div>";
+		String phraseData = phraseDataCells;
+		String phraseTable = "<div>" + phraseData + "</div>";
 
+		// Constructing final strings containing HTML for frequency and phrase match tables
 		String frequencyHTML = title + tableMain;
-		String phraseHTML = title + links;
+		String phraseHTML = title + links + phraseTable;
 
+
+		// Write to and close both files.
+		System.out.println("\n--------- Generating HTML file for Word Frequency results");
 		writerOne.write(frequencyHTML);
 		writerOne.close();
-
+		System.out.println("\n--------- Generating HTML file for Phrase Match results");
 		writerTwo.write(phraseHTML);
 		writerTwo.close();
 	}
 
-	// Find frequency values for each word mentioned in the files
+	// Loads the words, ranks them accordingly without duplicates, produces HTML strings containing
+	// relevant data.
 	public void populateFrequencyTable(int original) {
 		String resultsName;
 		String resultsOriginal;
@@ -51,7 +73,11 @@ public class Display {
 		load.toWords(original);
 		copyList();
 		frequency.rankWords(wordList);
+		System.out.println("\n--------- Sorting words in order");
 		frequency.removeDuplicates(wordList);
+		System.out.println("\n--------- Removing duplicate values");
+
+		// Produces a table row for each data point
 		for (int i = 0; i < wordList.size(); i++) {
 			resultsName = "<tr><td>" + wordList.get(i) + "</td>";
 			resultsOriginal = "<td>" + frequency.getFrequencyOne(i) + "</td>"; 
@@ -61,10 +87,40 @@ public class Display {
 		frequencyTableCells = tempList.toString().replaceAll("[ |,|\\[|\\]]", "");
 	}
 
+	// Constructs a HTML string containing all phrase match results, compares the phrases
+	// then constructs a table and a row for each given result from the files. 
 	public void populateMatchTable() throws IOException {
+		ArrayList filesOne = match.getFileList(1);
+		ArrayList filesTwo = match.getFileList(2);
+		System.out.println("\n--------- Comparing phrases from all available files");
 		match.comparePhrases();
+		tempList.clear();
+		String stepOne = "</br><table border=\"1\" style=\"width: 20%; margin: auto; width:20%\"><tr style=\"text-align: left\"><th>Pair</th><th>Phrase Match</th></tr>";
+		String stepThree = "</table></br>";
+
+		// Produces two table for all files
+		for (int i = 0; i < 10; i++) {
+			String stepTwo = "<tr><td>File " + filesOne.get(i) + " - File " + filesTwo.get(i) + "</td><td>" + match.getMatchList().get(i) + "</td></tr>";
+			if (i < 5) {
+				tableOne.add(stepTwo);
+			}
+			if (i >= 5) {
+				tableTwo.add(stepTwo);
+			}
+		}
+		match.rankMatches();
+		System.out.println("\n--------- Ranking match phrases");
+
+		// Produces a table for the highest ranking files in the sequence.
+		for (int i = 0; i < 5; i++) {
+			String stepTwo = "<tr><td>File " + filesOne.get(i) + " - File " + filesTwo.get(i) + "</td><td>" + match.getMatchList().get(i) + "</td></tr>";
+			tableThree.add(stepTwo);
+		}
+		tempList.add(stepOne + tableOne + stepThree + stepOne + tableTwo + stepThree + stepOne + tableThree + stepThree);
+		phraseDataCells = tempList.toString().replaceAll("[,|\\[|\\]]", "");
 	}
 
+	// Copies a list accessed from another class, making it global for this class.
 	public void copyList() {
 		for (int i = 0; i < load.getWords().size(); i++) {
 			wordList.add(load.getSingleWord(i));
--- Match.java
+++ Match.java
@@ -3,23 +3,32 @@ import java.util.*;
 
 public class Match {
 
+	// New instances of classes used
 	Frequency frequency = new Frequency();
 	Load load = new Load();
 	Load reload = new Load();
 
+	// Initialising Arraylists
 	ArrayList<Integer> matchList = new ArrayList<>();
+	ArrayList<Integer> fileOneList = new ArrayList<>();
+	ArrayList<Integer> fileTwoList = new ArrayList<>();
 
+	// Compares each combination of available files, searches for phrases that appear in both files
+	// Stores the results in an ArrayList in the form of Integers, can use getMatchList() function to
+	// access from another class.
 	public void comparePhrases() throws IOException {
-		int filesOne[] = {0, 0, 0, 0, 1, 1, 1, 2, 2, 3};
-		int filesTwo[] = {1, 2, 3, 4, 2, 3, 4, 3, 4, 4};
+		fileOneList.addAll(Arrays.asList(0, 0, 0, 0, 1, 1, 1, 2, 2, 3));
+		fileTwoList.addAll(Arrays.asList(1, 2, 3, 4, 2, 3, 4, 3, 4, 4));
 		int counter = 0;
+		// Compare each file against another using the given combination. 
 		for (int i = 0; i < 10; i++) {
-			load.toPhrase(filesOne[i]);
-			reload.toPhrase(filesTwo[i]);
+			load.toPhrase(fileOneList.get(i));
+			reload.toPhrase(fileTwoList.get(i));
 			int first = load.getPhrase().size();
 			int second = reload.getPhrase().size();
 			int loop = determineSize(first, second);
 			counter = 0;
+			// If phrase x of file on is repeated in file y then add to the counter
 			for (int j = 0; j < loop; j++) {
 				for (int k = 0; k < loop; k++) {
 					if (load.getPhrase().get(k).toString().equals(reload.getPhrase().get(j).toString())) {
@@ -27,11 +36,30 @@ public class Match {
 					}
 				}
 			}
+			// (Counter / total words) * 100.
 			matchList.add(Math.round((counter * 100.0f) / load.getPhrase().size()));
-			System.out.println(matchList.get(i));
+			System.out.println("\n-- Match found: " + matchList.get(i) + "%");
 		}
 	}
 
+	// Orders the ArrayLists for matches, and file combinations, shows highest to lowest.
+	// Can access the modified lists using the getFileList(index) accessor. Uses the insertion
+	// sort algorithm. 
+	public void rankMatches() {
+		for (int i = 1; i < matchList.size(); i++) {
+			int x = matchList.get(i);
+			for (int j = i - 1; j >= 0 && matchList.get(j) > x; j--) {
+				Collections.swap(matchList, j + 1, j);
+				Collections.swap(fileOneList, j + 1, j);
+				Collections.swap(fileTwoList, j + 1, j);
+			}
+		}
+		Collections.reverse(matchList);
+		Collections.reverse(fileOneList);
+		Collections.reverse(fileTwoList);
+	}
+
+	// Determines the size of the ArrayList used for matching.
 	public int determineSize(int first, int second) {
 		int loop = 0;
 		if (first > second) {
@@ -43,18 +71,22 @@ public class Match {
 		return loop;
 	}
 
-	public int getMatchList() {
+	// Accessor method for the matchList ArrayList returning the results of the phrase
+	// comparisons, given in Integer, should be represented as percentages. 
+	public ArrayList getMatchList() {
 		return matchList;
 	}
+
+	// Accessor method for the File combination ArrayLists, where @param index desired list 
+	// of files, either 1 or 2. Returns null if invalid index is given. 
+	public ArrayList getFileList(int index) {
+		ArrayList list = null;
+		if (index == 1) {
+			list = fileOneList;
+		}
+		if (index == 2) {
+			list = fileTwoList;
+		}
+		return list;
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
